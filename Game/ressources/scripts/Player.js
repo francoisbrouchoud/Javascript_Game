468f0a2de6230051756e1a213782f3907699f687
celine.vialard <celine.vialard@gmail.com>
2022-05-07 17:02:37 +0200

    move annimation player

--- Game/ressources/scripts/Player.js
+++ Game/ressources/scripts/Player.js
@@ -9,9 +9,14 @@ export default class Player {
     mission;
     day;
     isMoving;
+    moveInc=0;
+    switchMoveImg=0;
     hasHat; //Est-ce qu'il possède un chapeau ou pas ?
     // constante
-     imagePath = "ressources/images/Perso_principal.png";
+     persoPath = "ressources/images/Steve/SteveFace.jpg";
+     persoMvPath = "ressources/images/Steve/SteveDepl0.jpg";
+     persoHatPath = "ressources/images/Steve/SteveChapeau.jpg";
+     persoHatMvPath = "ressources/images/Steve/SteveDepl0ch.jpg";
      imageWidth=50;
      imageHeight=50;
      pasPerso = 5;
@@ -22,30 +27,36 @@ export default class Player {
         this.posX = w/2 -25;
         this.posY = h/2 -25;
       }
-    //qu'est-ce qu'il a besoin faire
     // déplacement
     move(direction){
         switch(direction){
             case 'w':
                 this.posY -= this.pasPerso;
+                this.isMoving=true;
                 break;
             case 'a':
                 this.posX -= this.pasPerso;
+                this.isMoving=true;
                 break;
             case 's':
                 this.posY += this.pasPerso;
+                this.isMoving=true;
                 break;
             case 'd':
                 this.posX += this.pasPerso;
+                this.isMoving=true;
                 break;
        }
+    }
+    stopMove(){
+        this.isMoving=false;
     }
     changePage(x,y){
         this.posX = x;
         this.posY = y; 
     }
     getPosition(){
-        return {x:this.posX ,y:this.posY}
+        return {x:this.posX ,y:this.posY, w:this.imageWidth, h:this.imageHeight}
     }
     // boire
     // dormir
@@ -54,8 +65,27 @@ export default class Player {
     draw(context){
 
         var image= new Image();
-        image.src = this.imagePath;
-    
+        if(!this.isMoving){
+            if(this.hasHat)
+                image.src = this.persoHatPath
+            else
+                image.src = this.persoPath;
+        }
+        else{
+            this.switchMoveImg++;
+
+            if(this.switchMoveImg >= 15){
+                this.moveInc++;
+                this.moveInc = this.moveInc%2;
+                this.switchMoveImg = 0;
+            }
+
+            if(this.hasHat)
+                image.src = this.persoHatMvPath.replace("0",this.moveInc)
+            else
+                image.src = this.persoMvPath.replace("0",this.moveInc);
+        }
+
         context.drawImage(image,this.posX,this.posY,this.imageWidth,this.imageHeight)
         context.stroke();
             
