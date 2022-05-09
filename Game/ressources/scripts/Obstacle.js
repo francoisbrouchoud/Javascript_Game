c6ef25392f93b0eb1a23ec25eccb0daa5d4b9deb
lucadelbuono <lucadelbuono2000@gmail.com>
2022-05-09 10:41:16 +0200

    Check collision between player and obstacle

--- Game/ressources/scripts/Obstacle.js
+++ Game/ressources/scripts/Obstacle.js
@@ -7,12 +7,30 @@ export default class Obstacle{
     active; //est-ce qu'il est activé ou pas, déclanche ou pas l'action
     action; //on va lui passer une fonction qu'il va executer quand il sera touché
 
-    constructor(posX, posY, height, width, active, action) {
+
+    constructor(posX, posY, width, height, active, action) {
         this.posX = posX;
         this.posY = posY;
-        this.height = height;
         this.width = width;
+        this.height = height;
         this.active = active;
         this.action = action;
+    }
+
+    getPosition(){
+        return {obsX:this.posX ,obsY:this.posY, obsW:this.width, obsH:this.height}
+    }
+
+    draw(context){
+
+        var image= new Image();
+        image.src = "ressources/images/Steve/SteveFace.jpg";
+
+
+        context.drawImage(image,this.posX,this.posY,this.width,this.height)
+        context.stroke();
+
+        //Restauration du context
+        context.restore();
     }
 }
