0e11be58fb4a3b831df574a804104c77a5634d8c
celine.vialard <celine.vialard@gmail.com>
2022-05-12 19:22:52 +0200

    update info class

--- Game/ressources/scripts/Room.js
+++ Game/ressources/scripts/Room.js
@@ -3,13 +3,11 @@ import Obstacle from './Obstacle.js';
 export default class Room{
     obsacle; // pour chaque obstacle, on va dire quelle action on va déclancher -> liste
 
-    wall; // tableau de bool pour savoir s'il peut passer ou pas 
     name;
     image;
 
-    constructor(name, image, wall, obsacle) {
+    constructor(name, image, obsacle) {
         this.obsacle = obsacle;
-        this.wall = wall;
         this.name = name;
         this.image = "ressources/images/Salles/" + image;
     }
@@ -20,7 +18,6 @@ export default class Room{
 // et ajouter dans le helper pour avoir une liste de fonction à écrire
 // à voir si pas mieux de les crée au niveau des missions pour les "mures"
 export const room1 = new Room("room1","gilliard.jpg",
-[null,room2,null,null],
 [
     new Obstacle(10,10,30,30,true,(player)=>{
         player.drink();
@@ -28,7 +25,6 @@ export const room1 = new Room("room1","gilliard.jpg",
 ]);
 
 export const room2 = new Room("room2","rhonefm.jpg",
-[null,room1,null,null],
 [
     new Obstacle(10,10,30,30,true,(player)=>{
         player.drink();
