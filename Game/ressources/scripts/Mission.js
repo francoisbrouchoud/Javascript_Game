21d68fef18a5ada34547a013a0b1cee3ccfb2fcb
François Brouchoud <francois.brouchoud@hevs.ch>
2022-05-16 09:24:56 +0200

    feat: add mission

--- Game/ressources/scripts/Mission.js
+++ Game/ressources/scripts/Mission.js
@@ -1,5 +1,30 @@
-import Task from './Task.js';
-import {room1, room2} from './Room.js';
+import Task, {
+    achatBilletEntree,
+    achatPot,
+    chapeauNouvelliste,
+    passerSecurite,
+    photoRhoneFm,
+    boirePorteDeNovembre,
+    retirerArgent,
+    roueSwisscom,
+    trouverAmiComptoir,
+    mangerFoodtruck,
+    boireSucreGpCornut,
+    degusterAbricool,
+    saluerPersonnalite, trouverAmiAfter, entrerAfter
+} from './Task.js';
+import {
+    abricoolRoom, afterRoom,
+    aleatoire1Room,
+    aleatoire2Room, aleatoire3Room,
+    entreeRoom, espaceGourmandRoom, gillardRoom, gpCornutRoom,
+    nouvellisteRoom,
+    potChambreRoom,
+    rhoneFmRoom,
+    room1,
+    room2, swisscomRoom
+} from './Room.js';
+import {abricool} from "./Obstacle";
 
 export default class Mission{
 
@@ -10,14 +35,20 @@ export default class Mission{
     constructor(title, task, rooms, startRoom) {
         this.title = title;
         this.task = task;
-        this.startRoom= startRoom;
         this.rooms = rooms;
+        this.startRoom = startRoom;
     }
 }
-/*
-export const mission1 = new Mission("Mission entrée",
-    [])
-/*
+
+export const missionEntree = new Mission("Mission entrée", [retirerArgent, achatBilletEntree, passerSecurite], [entreeRoom], entreeRoom);
+
+
+export const missionCerm1 = new Mission("Mission CERM 1", [trouverAmiComptoir, photoRhoneFm, chapeauNouvelliste, achatPot], [[null,nouvellisteRoom,null],[aleatoire1Room,aleatoire2Room,potChambreRoom],[null,null,rhoneFmRoom]], aleatoire1Room );
+export const missionCerm2 = new Mission("Mission CERM 2", [roueSwisscom, boirePorteDeNovembre], [[aleatoire3Room, null],[gillardRoom, aleatoire1Room],[null,swisscomRoom]], aleatoire3Room);
+export const missionExterieure = new Mission("Mission extérieure", [mangerFoodtruck, boireSucreGpCornut], [[aleatoire2Room, null],[gpCornutRoom, aleatoire1Room]], aleatoire2Room);
+export const missionEspaceGourmand = new Mission("Mission espace Gourmand", [degusterAbricool, saluerPersonnalite], [[abricoolRoom, null, null],[nouvellisteRoom, null, espaceGourmandRoom],[aleatoire1Room, swisscomRoom, aleatoire2Room]], abricoolRoom);
+export const missionAfter = new Mission("Mission After", [trouverAmiAfter, entrerAfter], [[aleatoire2Room, gpCornutRoom, null],[null, gillardRoom, afterRoom]], aleatoire2Room);
+
 
 /*
 export const mission1 = new Mission("mission 1",
