50094952e49b576bf40e2549af1f155b4f7b7cae
lucadelbuono <lucadelbuono2000@gmail.com>
2022-05-30 10:52:11 +0200

    New obstacle sizes

--- Game/ressources/scripts/Obstacle.js
+++ Game/ressources/scripts/Obstacle.js
@@ -27,105 +27,105 @@ export default class Obstacle{
 
 }
 
-export const abricool = new Obstacle(416,333,884,347, true, (game)=>{
+export const abricool = new Obstacle(138,111,294,115, true, (game)=>{
     game.player.drink();
     game.validationTask(28);
 });
 
-export const gillard = new Obstacle(416,333,884,347, true, (game)=>{
+export const gillard = new Obstacle(138,111,294,115, true, (game)=>{
     game.player.drink();
     game.validationTask(25);
 });
 
-export const glacier = new Obstacle(1343,342,226,325, true, (game)=>{
+export const glacier = new Obstacle(447,114,75,108, true, (game)=>{
     game.player.eat();
 });
 
-export const marrons = new Obstacle(270,1035,226,325, true, (game)=>{
+export const marrons = new Obstacle(90,345,75,108, true, (game)=>{
     game.player.eat();
 });
 
-export const bubbleTea = new Obstacle(515,1039,226,325, true, (game)=>{
+export const bubbleTea = new Obstacle(171,346,75,108, true, (game)=>{
     game.player.drink();
 });
 
-export const fondue = new Obstacle(826,1019,884,347, true, (game)=>{
+export const fondue = new Obstacle(275,340,295,116, true, (game)=>{
     game.player.eat();
 });
 
-export const after = new Obstacle(303,0,1541,595, true, (game)=>{
+export const after = new Obstacle(101,0,513,198, true, (game)=>{
     game.player.drink();
     game.validationTask(31);
 });
 
-export const espaceGourmand = new Obstacle(303,0,1541,595, true, (game)=>{
+export const espaceGourmand = new Obstacle(101,0,513,198, true, (game)=>{
     game.player.drink();
 });
 
 
-export const bancomat = new Obstacle(134,0,226,325, true, (game)=>{
+export const bancomat = new Obstacle(45,0,75,108, true, (game)=>{
     game.validationTask(3);
 });
 
-export const entreeComptoir = new Obstacle(571,0,977,381, true, (game)=>{
+export const entreeComptoir = new Obstacle(190,0,326,127, true, (game)=>{
     game.validationTask(2);
 });
 
-export const billeterie = new Obstacle(1064,929,920,354, true, (game)=>{
+export const billeterie = new Obstacle(355,310,307,118, true, (game)=>{
     game.validationTask(1);
 });
 
-export const swisscom = new Obstacle(346,853,920,354, true, (game)=>{
+export const swisscom = new Obstacle(115,284,307,118, true, (game)=>{
     game.validationTask(24);
 });
 
-export const gpCornut = new Obstacle(346,853,920,354, true, (game)=>{
+export const gpCornut = new Obstacle(115,284,307,118, true, (game)=>{
     game.player.drink();
     game.validationTask(27);
 });
 
-export const potDeChambre = new Obstacle(346,853,920,354, true, (game)=>{
+export const potDeChambre = new Obstacle(115,284,307,118, true, (game)=>{
     game.player.drink();
     game.validationTask(23);
 });
 
 
-export const nourriture = new Obstacle(946,284,920,354, true, (game)=>{
+export const nourriture = new Obstacle(315,95,307,118, true, (game)=>{
     game.player.eat();
     game.validationTask(26);
 });
 
-export const arret = new Obstacle(1243,29,479,313, true, (game)=>{
+export const arret = new Obstacle(414,10,160,104, true, (game)=>{
 });
 
-export const busPorteArriere = new Obstacle(887,723,91,257, true, (game)=>{
+export const busPorteArriere = new Obstacle(256,241,30,86, true, (game)=>{
 });
 
-export const busPorteAvant = new Obstacle(1193,889,91,257, true, (game)=>{
+export const busPorteAvant = new Obstacle(398,296,30,86, true, (game)=>{
 });
 
-export const nouvelliste = new Obstacle(661,317,920,354, true, (game)=>{
+export const nouvelliste = new Obstacle(220,106,307,118, true, (game)=>{
     game.validationTask(22);
 });
 
-export const rhoneFm = new Obstacle(661,317,920,354, true, (game)=>{
+export const rhoneFm = new Obstacle(220,106,307,118, true, (game)=>{
     game.validationTask(21);
 });
 
-export const friendPurple = new Obstacle(1435, 750, 101, 120, true, (game)=>{
+export const friendPurple = new Obstacle(478, 250, 33, 40, true, (game)=>{
     game.validationTask(11);
     game.validationTask(12);
     game.validationTask(13);
     game.validationTask(30);
 });
 
-export const friendGreen = new Obstacle(1029, 1211, 137, 169, true, (game)=>{
+export const friendGreen = new Obstacle(343, 404, 46, 56, true, (game)=>{
     game.validationTask(11);
     game.validationTask(12);
     game.validationTask(13);
     game.validationTask(30);
 });
 
-export const zonePersonnalites = new Obstacle(1317, 645, 399, 361, true, (game)=>{
+export const zonePersonnalites = new Obstacle(439, 215, 133, 120, true, (game)=>{
     game.validationTask(29);
 });
